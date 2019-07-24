6c9e1227736a09b242dac57b245ab64211f2de0c
hanif <hihanifm@gmail.com>
2019-07-24 09:13:19 -0400

    basics contd...

--- app.py
+++ app.py
@@ -4,3 +4,28 @@ print("Hello world ")
 print("Hello world")
 
 2 + 2
+
+a = 5
+
+
+b = 3
+
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = """
+multiple
+lines
+"""
+
+x, y = 1, 2
+
+x = y = 2
+
+print(type(students_count))
+
+type(bool)
+
+age = 20
+age = "Python"
+print(age)
